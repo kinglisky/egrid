fa745211831b5fb2694b2bef0268011e9635b06f
芊野 <free.nan.sky@gmail.com>
2018-01-05 23:57:27 +0800

    Update dependencies

--- dev/main.js
+++ dev/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import App from './App.vue'
-import 'element-ui/lib/theme-default/index.css'
-// import 'element-ui/lib/theme-chalk/index.css'
+// 1.4.12
+// import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 
